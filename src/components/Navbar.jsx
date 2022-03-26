bf980a25599637003c2df62d3a2fc735b99902e0
ensar <ensar.makas@gmail.com>
2022-03-26 19:41:12 +0300

    forgot to save Navbar :(

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
+import {Link} from 'react-router'
+
 function Navbar() {
     return (
         <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-            <div></div>
+            <div className="container flex flex-wrap justify-between items-center mx-auto">
+
+                <Link/>
+
+            </div>
         </nav>
     )
 
