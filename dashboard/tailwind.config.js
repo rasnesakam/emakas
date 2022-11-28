205dc1f30ee67cec05ea658870b948ec76030da4
ensar <ensar.makas@gmail.com>
2022-11-28 20:33:28 +0300

    created dashboard

---
+++ dashboard/tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+	"./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+		colors: {
+			ground:"#E0E0CE",
+			hover:"#FFD8A9",
+			primary:"#000",
+			secondary:"#F2AF29"
+		}
+	},
+	screens:{
+		'sm': '576px',
+		// => @media (min-width: 576px) { ... }
+
+		'md': '960px',
+		// => @media (min-width: 960px) { ... }
+
+		'lg': '1440px',
+		// => @media (min-width: 1440px) { ... }
+	}
+  },
+  plugins: [],
+}
