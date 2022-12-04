04c4dda25f28dd3468f192dda6e421892bc7ec3b
ensar <ensar.makas@gmail.com>
2022-12-04 20:48:16 +0300

    configured for ts

--- app/src/Routes/About/components/index.js
+++
@@ -1,5 +0,0 @@
-import { HeadSection } from "./sections/HeadSection";
-import { BodySection } from "./sections/BodySection";
-import { FootSection } from "./sections/FootSection";
-
-export {HeadSection, BodySection, FootSection}
--- app/src/Routes/About/components/sections/BodySection.js
+++
@@ -1,4 +0,0 @@
-
-const BodySection = () => {};
-
-export { BodySection };
