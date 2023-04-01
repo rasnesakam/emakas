7866d6ea4f74894ab338d9b78be115c7dcf498f6
ensar <ensar.makas@gmail.com>
2023-04-01 11:52:14 +0300

    change@component/about/head: Changed head section style

--- src/components/About/HeadSection.js
+++ src/components/About/HeadSection.js
@@ -6,13 +6,13 @@ const HeadSection = () => {
 	`;
 	const [user, setUSer] = useState({name:"Ensar Makas",message})
 
-	return <div className="bg-primary mh-screen  absolute left-0 right-0 flex flex-col md:flex-row">
+	return <div className="bg-onGround mh-screen  absolute left-0 right-0 flex flex-col md:flex-row">
 		<div className="">
 			<img className="w-full" src="https://avatars.githubusercontent.com/u/45129228?v=4" />
 		</div>
 		<div className="md:m-5 grid grid-flow-row grid-rows-3">
 			<div className="my-auto col-span-1 text-start">
-				<span className = "text-secondary text-3xl font-bold italic">{user.name}</span>
+				<span className = "text-primary text-3xl font-bold italic">{user.name}</span>
 			</div>
 			<div className=" col-span-2">
 				<span className = "text-secondary text-xl ">{user.message}</span>
