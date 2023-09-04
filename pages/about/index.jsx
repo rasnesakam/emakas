2eb6e34d0da976beade3c1784b7f7886145d071b
ensar <ensar.makas@gmail.com>
2023-09-04 19:32:26 +0300

    Changing code struct at about -1: HeadSection

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -3,7 +3,12 @@ import {HeadSection, BodySection, FootSection} from "../../src/components/About/
 const About = () => {
     return (<>
         
-		<HeadSection />
+		<HeadSection imgSrc={"https://avatars.githubusercontent.com/u/45129228?v=4"} title={"Ensar Makas"}>
+			<>
+				Merhaba. Ben Ensar, Beykent Üniversitesi bilgisayar mühendisliği mezunuyum <br />
+				Kod yazmayı ve tavuklu pilavı severim
+			</>
+		</HeadSection>
 		<BodySection />
 		<FootSection />
 		
--- src/components/About/HeadSection.js
+++ src/components/About/HeadSection.js
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Children } from "react";
 
-const HeadSection = () => {
+const HeadSection = ({imgSrc, title, children}) => {
 	let message = `
 	Merhaba. Ben Ensar. Beykent üniversitesi bilgisayar mühendisliği 4. sınıf öğrencisiyim. Resim yapmayı ve tevuklu pilavı severim
 	`;
@@ -8,14 +8,16 @@ const HeadSection = () => {
 
 	return <div className="bg-onGround mh-screen  absolute left-0 right-0 flex flex-col md:flex-row">
 		<div className="">
-			<img className="w-full" src="https://avatars.githubusercontent.com/u/45129228?v=4" />
+			<img className="w-full" src={imgSrc} />
 		</div>
 		<div className="md:m-5 grid grid-flow-row grid-rows-3">
 			<div className="my-auto col-span-1 text-start">
-				<span className = "text-primary text-3xl font-bold italic">{user.name}</span>
+				<span className = "text-primary text-3xl font-bold italic">{title}</span>
 			</div>
 			<div className=" col-span-2">
-				<span className = "text-secondary text-xl ">{user.message}</span>
+				<div className = "text-secondary text-xl ">
+					{children}
+				</div>
 			</div>
 		</div>
 
