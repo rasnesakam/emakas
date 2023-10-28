a1a5c425733f25e08ef320fd599098da5d54c13f
Ensar Makas <ensar.makas@gmail.com>
2023-10-29 00:07:50 +0300

    Added new Component for github feeds

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+import {GithubUserFeed} from '../src/components/Github/GithubUserFeed'
+
+export default function Index(){
+	
+	return (
+		<>
+			<GithubUserFeed />	
+		</>
+	);
+};
