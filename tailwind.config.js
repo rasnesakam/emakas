9cdda31e6b3ee44671718e07f7bd2ec596cc917d
Ensar Makas <ensar.makas@gmail.com>
2023-12-04 00:20:12 +0300

    Added media queries for screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,16 @@
                     primary:"#000000",
                     ground:"#FFFFFF",
                     secondary:"#d1d5db"
+                },
+                screens:{
+                    'sm': '576px',
+                    // => @media (min-width: 576px) { ... }
+
+                    'md': '960px',
+                    // => @media (min-width: 960px) { ... }
+
+                    'lg': '1440px',
+                    // => @media (min-width: 1440px) { ... }
                 }
             },
         },
