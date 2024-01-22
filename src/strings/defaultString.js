101f3b8d85919e08f5448f7553d520741dee2849
Ensar Makas <ensar.makas@gmail.com>
2024-01-22 22:51:36 +0300

    Added new strings

--- src/strings/defaultString.js
+++ src/strings/defaultString.js
@@ -1,5 +1,8 @@
 module.exports = {
     "see all": "see all",
     "work history":"work history",
-    "volunteer works": "volunteer works"
+    "volunteer works": "volunteer works",
+    "medium posts": "medium posts",
+    "education history": "education history",
+    "my projects": "my projects"
 }
