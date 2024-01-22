f7727be6bb7cb8d665fccf52d510f78e4b4f6753
Ensar Makas <ensar.makas@gmail.com>
2024-01-22 22:52:20 +0300

    Added formatters for define formats of strings
    Formats are:
    - Lowercase
    - Uppercase
    - Capitalized letters

--- src/strings/index.js
+++ src/strings/index.js
@@ -2,13 +2,23 @@ import stringsDefault from "./defaultString"
 import stringsTr from "./stringsTr"
 
 import {supportedLanguages} from "@/components/LanguageContextAdapter"
-function getString(key, language){
+
+const STRING_FORMATS = {
+    LOWER: (str) => str.toLowerCase(),
+    UPPER: (str) => str.toUpperCase(),
+    CAPITALIZED: (str) => str.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(" ")
+}
+function getString(key, language, format){
     switch (language){
-        case supportedLanguages.Turkish.lang:
+        case supportedLanguages.Turkish:
+            if (format !== undefined)
+                return format(stringsTr[key]);
             return stringsTr[key];
         default:
+            if (format !== undefined)
+                return format(stringsDefault[key]);
             return stringsDefault[key];
     }
 }
 
-export {getString}
+export {getString, STRING_FORMATS}
