3e38b0fcc6299850bf160144c7d5da8c04a8a0fe
Ensar Makas <ensar.makas@gmail.com>
2024-01-22 22:52:39 +0300

    Added nextui and dropdown components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-    module.exports = {
+const {nextui} = require("@nextui-org/theme");
+module.exports = {
         content: [
             './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
             './src/components/**/*.{js,ts,jsx,tsx,mdx}',
             './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+            './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
         ],
         theme: {
             extend: {
@@ -29,5 +31,5 @@
                 }
             },
         },
-        plugins: [],
+        plugins: [nextui()],
     }
