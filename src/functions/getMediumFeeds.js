3fae99284e195fbb1ba9ad2876af60bf94d913bd
Ensar Makas <ensar.makas@gmail.com>
2024-12-09 09:43:57 +0300

    medium feed'a no cache özelliği eklendi

--- src/functions/getMediumFeeds.js
+++ src/functions/getMediumFeeds.js
@@ -2,7 +2,7 @@
 export async function getMediumFeeds(username){
     const mediumLink = `https://medium.com/@${username}/feed`;
     let parser = require("xml2js")
-    let mediumFeeds = await fetch(mediumLink).then(response => response.text())
+    let mediumFeeds = await fetch(mediumLink, {cache: "no-store"}).then(response => response.text())
     let mediumFeedsJson = {}
     parser.parseString(mediumFeeds,function(err,result){
         if (err != null){
