b5555e971b9f652911852eb5f4078318aa584913
Ensar Makas <ensar.makas@gmail.com>
2024-12-24 10:14:16 +0300

    Fixed client side errors

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
     const about = db[language.lang].about;
 
     useEffect(() => {
-        getMediumFeedsApi().then(feeds => setMediumBlogs(feeds));
+        getMediumFeedsApi().then(feeds => setMediumBlogs(feeds.content.mediumFeedsJson.rss.channel[0]));
         return () => {
             setMediumBlogs([])
         }
