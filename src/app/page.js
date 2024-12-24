06e6628167a3c88711ed9fa4b739de5f371b7f3b
Ensar Makas <ensar.makas@gmail.com>
2024-12-24 10:55:36 +0300

    TIP-ONE

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
     const about = db[language.lang].about;
 
     useEffect(() => {
-        getMediumFeedsApi().then(feeds => setMediumBlogs(feeds.content.mediumFeedsJson.rss.channel[0].item));
+        getMediumFeedsApi().then(feeds => setMediumBlogs(feeds.content.mediumFeedsJson.rss.channel[0].item)).then((_) =>console.log(mediumBlogs));
         return () => {
             setMediumBlogs([])
         }
