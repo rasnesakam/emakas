0df7df963eb7fb8b53cf171f822026813e7c2ea0
Ensar Makas <ensar.makas@gmail.com>
2025-05-07 14:35:32 +0300

    resolved conflicts

---
+++ src/data/about/en.js
@@ -0,0 +1,19 @@
+const about = {
+    title: "Hello, I'm Ensar Makas.",
+    jobTitle:"Software Engineer",
+    content: `
+    I am passionate about the internet and internet technologies, continuously developing my skills in this field.
+    Throughout my university education, I took part in various activities and actively sought professional experience.
+    My goal is to develop performance-oriented solutions in the field of internet technologies, considering the industry's needs and ensuring top-notch user and developer experiences.
+    `,
+    sections: [
+        {
+            title: "What do I do",
+            description: `
+            
+            `,
+        }
+    ]
+};
+
+export {about}
---
+++ src/data/volunteerWorks/en.js
@@ -0,0 +1,16 @@
+const volunteerWorks = [
+    {
+        title: "Chairman of the Administrative Board, IEEE Beykent University Computer Society",
+        description: "During my term as the chairman of the administrative board, we organized competitions and seminars.",
+        startDate: "August 2021",
+        endDate: "July 2022"
+    },
+    {
+        title: "Active Member, IEEE Beykent University Student Branch",
+        description: "During my active membership, I participated in various committees and projects.",
+        startDate: "August 2020",
+        endDate: "July 2021"
+    }
+];
+
+export {volunteerWorks}
